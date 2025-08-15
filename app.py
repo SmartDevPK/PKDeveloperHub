e30dc98a948d663a029ddc08dbb25a7376d5cde5
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-08-15 01:55:30 +0100

    Connect mongodb to the code base

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+mongo = PyMongo(app)
 
 
 if __name__ == "__main__":
---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+
+#Mongodb connection URI
+MONGO_URI = os.environ.get("mongodb://localhost:27017/PKDEVELOPERHUB")
